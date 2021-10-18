64a3a0466477a5c810f65d1df59ef9ac38a6fc94
carrielies <carrielies@users.noreply.github.com>
2021-10-18 13:45:19 +0100

    CTCTRADERS-2733 Add Redirects

---
+++ test/controllers/RedirectControllerSpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+
+import scala.concurrent.Future
+
+class RedirectControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
+
+  "return OK and the correct view for a GET" in {
+
+    when(mockRenderer.render(any(), any())(any()))
+      .thenReturn(Future.successful(Html("")))
+
+    val request = FakeRequest(GET, routes.RedirectController.onPageLoad().url)
+
+    val result = route(app, request).value
+
+    status(result) mustEqual SEE_OTHER
+    redirectLocation(result).value mustEqual routes.WhatDoYouWantToDoController.onPageLoad().url
+  }
+}
