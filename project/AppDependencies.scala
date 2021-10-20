f090ec8837d8162aacb236dcef49ba4728c0e529
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2021-10-20 15:17:14 +0100

    dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "org.reactivemongo"          %% "play2-reactivemongo"             % "0.20.13-play28",
+    "org.reactivemongo"          %% "reactivemongo-play-json-compat"  % "0.20.13-play28",
     "uk.gov.hmrc"                %% "logback-json-logger"             % "5.1.0",
     "uk.gov.hmrc"                %% "play-conditional-form-mapping"   % "1.9.0-play-28",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"      % "5.16.0",
     "uk.gov.hmrc"                %% "play-allowlist-filter"           % "1.0.0-play-28",
     "uk.gov.hmrc"                %% "play-nunjucks"                   % "0.32.0-play-28",
     "uk.gov.hmrc"                %% "play-nunjucks-viewmodel"         % "0.15.0-play-28",
+    "com.typesafe.play"          %% "play-iteratees"                  % "2.6.1",
+    "com.typesafe.play"          %% "play-iteratees-reactive-streams" % "2.6.1",
     "org.webjars.npm"            % "govuk-frontend"                   % "3.13.0",
     "org.webjars.npm"            % "hmrc-frontend"                    % "1.22.0",
     "com.lucidchart"             %% "xtract"                          % "2.2.1"
