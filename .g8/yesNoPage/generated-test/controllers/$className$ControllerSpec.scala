8fcd7e664cb11dc91b5aa30c27f2fad1a399da00
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2021-10-21 10:28:43 +0100

    Removed scaffold session repository

--- .g8/yesNoPage/generated-test/controllers/$className$ControllerSpec.scala
+++ .g8/yesNoPage/generated-test/controllers/$className$ControllerSpec.scala
@@ -93,15 +93,12 @@ class $className$ControllerSpec extends SpecBase with MockitoSugar with Nunjucks
 
     "must redirect to the next page when valid data is submitted" in {
 
-      val mockSessionRepository = mock[SessionRepository]
-
       when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
 
       val application =
         applicationBuilder(userAnswers = Some(emptyUserAnswers))
           .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute))
           )
           .build()
 
