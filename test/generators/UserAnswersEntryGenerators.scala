f33b293cc1807eca6d35e424f671a6843fd9b0f8
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2021-10-21 10:20:44 +0100

    spacing

--- test/generators/UserAnswersEntryGenerators.scala
+++ test/generators/UserAnswersEntryGenerators.scala
@@ -21,4 +21,7 @@ import org.scalacheck.Arbitrary.arbitrary
 import pages._
 import play.api.libs.json.{JsValue, Json}
 
-trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {}
+trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
+
+  
+}
