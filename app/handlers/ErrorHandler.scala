69f87a228898d6e01bae222aec49605834363421
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2022-01-24 09:00:01 +0000

    CTCTRADERS-2904: Fixed failing content for ErrorHandler

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -45,15 +45,12 @@ class ErrorHandler @Inject() (
     implicit val rh: RequestHeader = request
 
     statusCode match {
-      case BAD_REQUEST =>
-        renderer.render("badRequest.njk").map(BadRequest(_))
       case NOT_FOUND =>
         renderer.render("notFound.njk", Json.obj()).map(NotFound(_))
+      case result if isClientError(result) =>
+        renderer.render("badRequest.njk").map(Results.Status(statusCode)(_))
       case _ =>
-        renderer.render("error.njk", Json.obj()).map {
-          content =>
-            Results.Status(statusCode)(content)
-        }
+        renderer.render("technicalDifficulties.njk").map(Results.Status(statusCode)(_))
     }
   }
 
