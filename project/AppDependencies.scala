8314e5c62c23c2ee8303c9a9ad648c164d23e102
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-05-10 16:18:13 +0100

    update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoVersion = "1.0.0"
-  private val bootstrapVersion = "7.12.0"
+  private val mongoVersion = "1.2.0"
+  private val bootstrapVersion = "7.15.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-28"              % mongoVersion,
-    "uk.gov.hmrc"                %% "play-conditional-form-mapping"   % "1.12.0-play-28",
+    "uk.gov.hmrc"                %% "play-conditional-form-mapping"   % "1.13.0-play-28",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"      % bootstrapVersion,
     "uk.gov.hmrc"                %% "play-allowlist-filter"           % "1.1.0",
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"              % "6.0.0-play-28",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"              % "7.7.0-play-28",
     "org.typelevel"              %% "cats-core"                       % "2.9.0"
   )
 
