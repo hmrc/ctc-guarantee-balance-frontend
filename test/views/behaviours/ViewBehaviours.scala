96205603cc4158566fb2f05fbb12ebe890ede316
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-05-12 14:05:50 +0100

    CTCP-2837: Added custom user research banner.

--- test/views/behaviours/ViewBehaviours.scala
+++ test/views/behaviours/ViewBehaviours.scala
@@ -148,8 +148,7 @@ trait ViewBehaviours extends SpecBase with ViewSpecAssertions {
 
   private def pageWithButton(expectedText: String)(additionalAssertions: Element => Assertion*): Unit =
     s"must render $expectedText button" in {
-      val button = getElementByClass(doc, "govuk-button")
-      assertElementContainsText(button, expectedText)
+      val button = doc.getElementsByClass("govuk-button").toList.find(_.text() == expectedText).value
       additionalAssertions.map(_(button))
     }
 
