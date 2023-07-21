e27f27b502076e85f2fee2ebd0fbfc0ad425f1ce
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-07-21 12:11:05 +0100

    CTCP-2812: Removed balanceId as not needed for P5

--- test/controllers/v2/TryAgainControllerV2Spec.scala
+++ test/controllers/v2/TryAgainControllerV2Spec.scala
@@ -18,27 +18,16 @@ package controllers.v2
 
 import base.{AppWithDefaultMockFixtures, SpecBase}
 import controllers.routes
-import models.UserAnswers
-import models.values.BalanceId
-import org.scalacheck.Arbitrary.arbitrary
-import pages.BalanceIdPage
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.{Authorization, HeaderCarrier}
 import views.html.v2.TryAgainViewV2
 
-import java.util.UUID
-
 class TryAgainControllerV2Spec extends SpecBase with AppWithDefaultMockFixtures {
 
   override protected def applicationBuilder(): GuiceApplicationBuilder =
     super.v2ApplicationBuilder()
-
-  private val expectedUuid: UUID   = arbitrary[UUID].sample.value
-  private val balanceId: BalanceId = BalanceId(expectedUuid)
-
-  private val baseAnswers: UserAnswers = emptyUserAnswers.setValue(BalanceIdPage, balanceId)
 
   implicit val hc: HeaderCarrier = HeaderCarrier(Some(Authorization("BearerToken")))
 
@@ -46,31 +35,16 @@ class TryAgainControllerV2Spec extends SpecBase with AppWithDefaultMockFixtures 
 
     "onLoad" - {
       "must return OK and the correct view for a GET" - {
-        "when balance ID exists in user answers" in {
-          setExistingUserAnswers(baseAnswers)
+        setExistingUserAnswers(emptyUserAnswers)
 
-          val request = FakeRequest(GET, routes.TryAgainController.onPageLoad().url)
-          val view    = injector.instanceOf[TryAgainViewV2]
-          val result  = route(app, request).value
+        val request = FakeRequest(GET, routes.TryAgainController.onPageLoad().url)
+        val view    = injector.instanceOf[TryAgainViewV2]
+        val result  = route(app, request).value
 
-          status(result) mustEqual OK
+        status(result) mustEqual OK
 
-          contentAsString(result) mustEqual
-            view(Some(balanceId.value))(request, messages).toString
-        }
-
-        "when balance ID doesn't exist in user answers" in {
-          setExistingUserAnswers(emptyUserAnswers)
-
-          val request = FakeRequest(GET, routes.TryAgainController.onPageLoad().url)
-          val view    = injector.instanceOf[TryAgainViewV2]
-          val result  = route(app, request).value
-
-          status(result) mustEqual OK
-
-          contentAsString(result) mustEqual
-            view(None)(request, messages).toString
-        }
+        contentAsString(result) mustEqual
+          view(None)(request, messages).toString
       }
     }
   }
