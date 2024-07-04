88dbbec441de16ed0098bc6d0ea03d1affb87ecf
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-04 07:40:59 +0100

    CTCP-5506: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                      % mongoVersion,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "2.0.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "9.11.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.3.0",
     "org.typelevel"              %% "cats-core"                               % "2.10.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"                     % "8.0.0",
     "org.apache.pekko"           %% "pekko-actor"                             % pekkoVersion,
