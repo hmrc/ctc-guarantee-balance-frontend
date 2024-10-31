b82249b5259cd1504d50eee51c9c8c1e5a80c0a1
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-10-31 16:11:15 +0000

    CTCP-5989: Update play-frontend dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.2.0"
+  private val mongoVersion = "2.3.0"
   private val bootstrapVersion = "9.5.0"
   private val pekkoVersion = "1.0.3"
 
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                      % mongoVersion,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "10.13.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "11.2.0",
     "org.typelevel"              %% "cats-core"                               % "2.12.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"                     % "8.1.0",
     "org.apache.pekko"           %% "pekko-actor"                             % pekkoVersion,
