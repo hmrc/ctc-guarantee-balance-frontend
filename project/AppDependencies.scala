8f89421f1c8ac9b724ae194edb456d0ed68012ca
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-05-20 11:53:03 +0100

    CTCP-6485 Update dependencies
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.4.0"
-  private val bootstrapVersion = "9.6.0"
+  private val mongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.12.0"
   private val pekkoVersion = "1.0.3"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                      % mongoVersion,
-    "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.2.0",
+    "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "11.9.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.1.0",
     "org.typelevel"              %% "cats-core"                               % "2.12.0",
-    "uk.gov.hmrc"                %% "crypto-json-play-30"                     % "8.1.0",
+    "uk.gov.hmrc"                %% "crypto-json-play-30"                     % "8.2.0",
     "org.apache.pekko"           %% "pekko-actor"                             % pekkoVersion,
     "org.apache.pekko"           %% "pekko-stream"                            % pekkoVersion
   )
