1c7d4cb3ed3a7c8504fbc0da94473892fa25e62c
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-08-26 11:36:10 +0100

    CTCP6-50: Update bootstrap version
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val mongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.14.0"
+  private val bootstrapVersion = "9.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
