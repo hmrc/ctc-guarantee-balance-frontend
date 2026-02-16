49dd93d6b2c5ad8070341f47f577ccade76fe665
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-16 11:38:59 +0000

    Upgrade libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.7.0"
-  private val bootstrapVersion = "9.19.0"
+  private val mongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
