17a95b516122db24490c39c6ebf01a69b62aa854
geethumathew <geethuelizebeth@gmail.com>
2026-08-13 11:41:36 +0100

    DL-20025:Pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.12.0"
+  private val mongoVersion = "2.13.0"
   private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
