b8441394466d1c2f51e8553cf9d0905d8e7477c2
geethumathew <geethuelizebeth@gmail.com>
2026-08-20 09:56:09 +0100

    DL-20025:Correct failing test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                      % mongoVersion,
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "12.32.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"              % "13.1.0",
     "org.typelevel"              %% "cats-core"                               % "2.13.0",
     "uk.gov.hmrc"                %% "crypto-json-play-30"                     % "8.4.0"
   )
